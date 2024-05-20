d5bd476e417ea56375fe1f5756eaff3cdd348e34
CristianN01 <cristiannechifor71@gmail.com>
2024-05-20 14:42:03 +0200

    creato funzione per randomNumber

--- JS/script.js
+++ JS/script.js
@@ -3,24 +3,32 @@ const playButton = document.querySelector('button.play')
 
 // ? <article class="cell"></article>
 
-
 playButton.addEventListener('click', function(){
     document.querySelector('section.container').innerHTML = "";
     
     for (let index = 0; index < 100; index++) {
-
-    const cellElement = document.createElement('article');
-    cellElement.classList.add('cell')
-    cellElement.append(index + 1)
-    cellElement.addEventListener('click', function(){
-        cellElement.classList.toggle('active')
-        console.log(index + 1)
-    });
-
-    
-    gridContainer.appendChild(cellElement)
-}
+        
+        const cellElement = document.createElement('article');
+        cellElement.classList.add('cell')
+        cellElement.append(index + 1)
+        cellElement.addEventListener('click', function(){
+            cellElement.classList.toggle('active')
+            console.log(index + 1)
+        });
+        
+        
+        gridContainer.appendChild(cellElement)
+    }
 });
+
+const bomb =[];
+const numbersList = [];
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
 
 
 
